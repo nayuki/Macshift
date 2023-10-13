5b1b604aa4b2895b7cbc95e737d7021ef87a83a6
qitamic <95576417+qitamic@users.noreply.github.com>
2023-10-13 14:49:25 +0800

    Accepting MAC with dash, colon, and space

--- macshift.cpp
+++ macshift.cpp
@@ -1,21 +1,21 @@
-/* 
+/*
  * Macshift - the simple Windows MAC address changing utility
- * 
+ *
  * Copyright (c) 2004 Nathan True <macshift@natetrue.com>
  * http://www.natetrue.com/
  * Copyright (c) Project Nayuki
  * https://www.nayuki.io/page/macshift-nayukis-version
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
@@ -36,44 +36,45 @@
 #include <netcon.h>
 
 
-// Function prototypes
-static void submain(const std::vector<std::string> &argVec);
-static void showHelp(const std::string &exePath);
-static bool isValidMac(const std::string &str);
+ // Function prototypes
+static void submain(const std::vector<std::string>& argVec);
+static void showHelp(const std::string& exePath);
+static std::string formatMac(const std::string& str);
+static bool isValidMac(const std::string& str);
 static std::string randomMac();
-static std::string findAdapterId(const std::string &adapterName);
-static void setMac(const std::string &adapterId, const std::string &newMac);
-static void resetAdapter(const std::string &AdapterName);
+static std::string findAdapterId(const std::string& adapterName);
+static void setMac(const std::string& adapterId, const std::string& newMac);
+static void resetAdapter(const std::string& AdapterName);
 
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
 	std::cerr << "Macshift v2.0 - the simple Windows MAC address changing utility" << std::endl;
-	
+
 	std::vector<std::string> argVec;
 	for (int i = 0; i < argc; i++)
 		argVec.push_back(std::string(argv[i]));
-	
+
 	srand(static_cast<unsigned int>(GetTickCount64()));
-	
+
 	try {
 		submain(argVec);
 	} catch (const std::exception &e) {
 		std::cerr << "Error: " << e.what() << std::endl;
 		return EXIT_FAILURE;
 	}
-	
+
 	return EXIT_SUCCESS;
 }
 
 
-static void submain(const std::vector<std::string> &argVec) {
+static void submain(const std::vector<std::string>& argVec) {
 	std::string adapterName = "";
 	bool isMacModeSet = false;
 	std::string newMac = randomMac();
-	
+
 	// Parse command-line arguments
 	for (std::size_t i = 1; i < argVec.size(); i++) {
-		const std::string &arg = argVec.at(i);
+		const std::string& arg = argVec.at(i);
 		if (arg.find("-") == 0) {  // A flag
 			if (arg == "-h")
 				showHelp(argVec.at(0));
@@ -89,7 +90,7 @@ static void submain(const std::vector<std::string> &argVec) {
 					if (argVec.size() - i <= 1)
 						throw std::invalid_argument("Missing MAC address argument");
 					i++;
-					const std::string &val = argVec.at(i);
+					std::string val = formatMac(argVec.at(i));
 					if (!isValidMac(val))
 						throw std::invalid_argument("Invalid MAC address, must match pattern /[0-9a-fA-F]{12}/");
 					newMac = val;
@@ -103,10 +104,10 @@ static void submain(const std::vector<std::string> &argVec) {
 			adapterName = arg;
 		}
 	}
-	
+
 	if (adapterName == "")
 		showHelp(argVec.at(0));
-	
+
 	std::cerr << "New MAC address: ";
 	if (newMac == "")
 		std::cerr << "(restore)";
@@ -118,7 +119,7 @@ static void submain(const std::vector<std::string> &argVec) {
 		}
 	}
 	std::cerr << std::endl;
-	
+
 	std::string adapterId = findAdapterId(adapterName);
 	std::cerr << "Network adapter ID: " << adapterId << std::endl;
 	setMac(adapterId, newMac);
@@ -126,15 +127,15 @@ static void submain(const std::vector<std::string> &argVec) {
 }
 
 
-static void showHelp(const std::string &exePath) {
+static void showHelp(const std::string& exePath) {
 	std::cerr << "https://www.nayuki.io/page/macshift-nayukis-version" << std::endl;
 	std::cerr << std::endl;
 	std::cerr << "Usage: " << exePath << " AdapterName [Options]" << std::endl;
 	std::cerr << std::endl;
 	std::cerr << "Example: " << exePath << " \"Wi-Fi\" -r" << std::endl;
 	std::cerr << "Example: " << exePath << " \"Ethernet\" -a 02ABCDEF9876" << std::endl;
-	
-	const std::vector<const char *> LINES{
+
+	const std::vector<const char*> LINES{
 		"",
 		"Options:",
 		"    -r               Use a random MAC address (default action).",
@@ -149,21 +150,32 @@ static void showHelp(const std::string &exePath) {
 		"When you change a MAC address, all your connections",
 		"are closed automatically and your adapter is reset.",
 	};
-	for (const char *line : LINES)
+	for (const char* line : LINES)
 		std::cerr << line << std::endl;
-	
+
 	std::exit(EXIT_FAILURE);
 }
 
 
-static bool isValidMac(const std::string &str) {
+static std::string formatMac(const std::string& str) {
+	std::string s = "";
+	for (int i = 0; i < str.size(); i++) {
+		char c = str.at(i);
+		if (c != ':' && c != '-' && c != ' ')
+			s += c;
+	}
+	return s;
+}
+
+
+static bool isValidMac(const std::string& str) {
 	if (str.size() != 12)
 		return false;
 	for (int i = 0; i < 12; i++) {
 		char c = str.at(i);
 		bool ok = ('0' <= c && c <= '9')
-		       || ('a' <= c && c <= 'f')
-		       || ('A' <= c && c <= 'F');
+			|| ('a' <= c && c <= 'f')
+			|| ('A' <= c && c <= 'F');
 		if (!ok)
 			return false;
 	}
@@ -179,7 +191,7 @@ static std::string randomMac() {
 			b = (b & 0xFC) | 0x02;  // Set local and unicast bits
 		temp = (temp << 8) | b;
 	}
-	
+
 	static const std::string HEX_DIGITS = "0123456789ABCDEF";
 	std::string result;
 	for (int i = 11; i >= 0; i--) {
@@ -193,12 +205,12 @@ static std::string randomMac() {
 // https://stackoverflow.com/questions/161177/does-c-support-finally-blocks-and-whats-this-raii-i-keep-hearing-about/25510879#25510879
 template <typename F>
 class Finally {
-	private: F func;
-	public: Finally(F f) :
-		func(f) {}
-	public: ~Finally() {
-		func();
-	}
+private: F func;
+public: Finally(F f) :
+	func(f) {}
+public: ~Finally() {
+	func();
+}
 };
 template <typename F>
 static Finally<F> finally(F f) {
@@ -209,12 +221,12 @@ static Finally<F> finally(F f) {
 static constexpr std::size_t TEXT_BUFFER_LEN = 512;
 
 
-static std::string findAdapterId(const std::string &adapterName) {
+static std::string findAdapterId(const std::string& adapterName) {
 	HKEY hListKey;
 	if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, "SYSTEM\\CurrentControlSet\\Control\\Network\\{4D36E972-E325-11CE-BFC1-08002BE10318}", 0, KEY_READ, &hListKey) != ERROR_SUCCESS)
 		throw std::runtime_error("Failed to open adapter list key");
-	auto hListKeyFinally = finally([hListKey]{ RegCloseKey(hListKey); });
-	
+	auto hListKeyFinally = finally([hListKey] { RegCloseKey(hListKey); });
+
 	for (DWORD i = 0; ; i++) {
 		std::string nameStr;
 		{
@@ -227,17 +239,17 @@ static std::string findAdapterId(const std::string &adapterName) {
 				throw std::runtime_error("Failed to enumerate registry keys");
 			nameStr = name.data();
 		}
-		
+
 		std::string subkey = nameStr + "\\Connection";
 		HKEY hKey;
 		if (RegOpenKeyEx(hListKey, subkey.c_str(), 0, KEY_READ, &hKey) != ERROR_SUCCESS)
 			continue;
-		auto hKeyFinally = finally([hKey]{ RegCloseKey(hKey); });
-		
+		auto hKeyFinally = finally([hKey] { RegCloseKey(hKey); });
+
 		std::vector<char> value(TEXT_BUFFER_LEN);
 		DWORD valueLen = static_cast<DWORD>(value.size());
 		if (RegQueryValueEx(hKey, "Name", nullptr, nullptr, reinterpret_cast<LPBYTE>(value.data()), &valueLen) == ERROR_SUCCESS
-				&& std::string(value.data()) == adapterName) {
+			&& std::string(value.data()) == adapterName) {
 			return nameStr;
 		}
 	}
@@ -245,12 +257,12 @@ static std::string findAdapterId(const std::string &adapterName) {
 }
 
 
-static void setMac(const std::string &adapterId, const std::string &newMac) {
+static void setMac(const std::string& adapterId, const std::string& newMac) {
 	HKEY hListKey;
 	if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, "SYSTEM\\CurrentControlSet\\Control\\Class\\{4D36E972-E325-11CE-BFC1-08002BE10318}", 0, KEY_READ, &hListKey) != ERROR_SUCCESS)
 		throw std::runtime_error("Failed to open adapter list key in Phase 2");
-	auto hListKeyFinally = finally([hListKey]{ RegCloseKey(hListKey); });
-	
+	auto hListKeyFinally = finally([hListKey] { RegCloseKey(hListKey); });
+
 	for (DWORD i = 0; ; i++) {
 		std::vector<char> name(TEXT_BUFFER_LEN);
 		{
@@ -261,17 +273,17 @@ static void setMac(const std::string &adapterId, const std::string &newMac) {
 			if (stat != ERROR_SUCCESS)
 				throw std::runtime_error("Failed to enumerate registry keys");
 		}
-		
+
 		HKEY hKey;
 		if (RegOpenKeyEx(hListKey, name.data(), 0, KEY_READ | KEY_SET_VALUE, &hKey) != ERROR_SUCCESS)
 			continue;
-		auto hKeyFinally = finally([hKey]{ RegCloseKey(hKey); });
-		
+		auto hKeyFinally = finally([hKey] { RegCloseKey(hKey); });
+
 		std::vector<char> value(TEXT_BUFFER_LEN);
 		DWORD valueLen = static_cast<DWORD>(value.size());
 		if (RegQueryValueEx(hKey, "NetCfgInstanceId", nullptr, nullptr, reinterpret_cast<LPBYTE>(value.data()), &valueLen) == ERROR_SUCCESS
-				&& std::string(value.data()) == adapterId) {
-			if (RegSetValueEx(hKey, "NetworkAddress", 0, REG_SZ, reinterpret_cast<const BYTE *>(newMac.c_str()), static_cast<DWORD>(newMac.size() + 1)) != ERROR_SUCCESS)
+			&& std::string(value.data()) == adapterId) {
+			if (RegSetValueEx(hKey, "NetworkAddress", 0, REG_SZ, reinterpret_cast<const BYTE*>(newMac.c_str()), static_cast<DWORD>(newMac.size() + 1)) != ERROR_SUCCESS)
 				throw std::runtime_error("Failed to set registry key");
 			std::cerr << "Wrote registry key" << std::endl;
 			return;  // Success
@@ -281,38 +293,38 @@ static void setMac(const std::string &adapterId, const std::string &newMac) {
 }
 
 
-static void resetAdapter(const std::string &adapterName) {
+static void resetAdapter(const std::string& adapterName) {
 	HMODULE netshellLib = LoadLibrary("Netshell.dll");
 	if (netshellLib == nullptr)
 		throw std::runtime_error("Failed to load Netshell.dll");
-	auto netshellLibFinally = finally([netshellLib]{ FreeLibrary(netshellLib); });
-	
-	auto NcFreeNetConProperties = reinterpret_cast<void (__stdcall *)(struct tagNETCON_PROPERTIES *)>(
+	auto netshellLibFinally = finally([netshellLib] { FreeLibrary(netshellLib); });
+
+	auto NcFreeNetConProperties = reinterpret_cast<void(__stdcall*)(struct tagNETCON_PROPERTIES*)>(
 		GetProcAddress(netshellLib, "NcFreeNetconProperties"));
 	if (NcFreeNetConProperties == nullptr)
 		throw std::runtime_error("Failed to load function from DLL");
-	
+
 	std::wstring buf;
 	for (std::size_t i = 0; i < adapterName.size(); i++)
 		buf.push_back(static_cast<wchar_t>(adapterName.at(i)));
-	
+
 	(void)CoInitialize(nullptr);
-	auto comFinally = finally([]{ CoUninitialize(); });
-	
-	INetConnectionManager *conMgr;
-	struct _GUID guid = {0xBA126AD1, 0x2166, 0x11D1, {0xB1,0xD0,0x00,0x80,0x5F,0xC1,0x27,0x0E}};
+	auto comFinally = finally([] { CoUninitialize(); });
+
+	INetConnectionManager* conMgr;
+	struct _GUID guid = { 0xBA126AD1, 0x2166, 0x11D1, {0xB1,0xD0,0x00,0x80,0x5F,0xC1,0x27,0x0E} };
 	if (::CoCreateInstance(guid, nullptr, CLSCTX_ALL, __uuidof(INetConnectionManager), reinterpret_cast<void**>(&conMgr)) != S_OK)
 		throw std::runtime_error("Failed to create connection manager");
-	auto conMgrFinally = finally([conMgr]{ conMgr->Release(); });
-	
-	IEnumNetConnection *enumCon;
+	auto conMgrFinally = finally([conMgr] { conMgr->Release(); });
+
+	IEnumNetConnection* enumCon;
 	conMgr->EnumConnections(NCME_DEFAULT, &enumCon);
 	if (enumCon == nullptr)
 		throw std::runtime_error("Could not enumerate Network Connections");
-	auto enumConFinally = finally([enumCon]{ enumCon->Release(); });
-	
+	auto enumConFinally = finally([enumCon] { enumCon->Release(); });
+
 	while (true) {
-		INetConnection *netCon;
+		INetConnection* netCon;
 		{
 			ULONG fetched;
 			enumCon->Next(1, &netCon, &fetched);
@@ -321,13 +333,13 @@ static void resetAdapter(const std::string &adapterName) {
 		}
 		if (netCon == nullptr)
 			continue;
-		
-		NETCON_PROPERTIES *conProp;
+
+		NETCON_PROPERTIES* conProp;
 		netCon->GetProperties(&conProp);
 		if (conProp == nullptr)
 			continue;
-		auto conPropFinally = finally([conProp, NcFreeNetConProperties]{ NcFreeNetConProperties(conProp); });
-		
+		auto conPropFinally = finally([conProp, NcFreeNetConProperties] { NcFreeNetConProperties(conProp); });
+
 		if (std::wcscmp(conProp->pszwName, buf.c_str()) == 0) {
 			std::cerr << "Resetting adapter" << std::endl;
 			netCon->Disconnect();
